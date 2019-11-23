bc613e7b775e0c164feb28dc47bcfe3b11e879d6
Ray McClain <rmcclain@decibelinsight.com>
2019-11-23 13:38:57 +0000

    Initial Commit

---
+++ Assets/ShipController.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShipController : MonoBehaviour {
+
+	public int speed = 5;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		transform.Translate(Vector3.forward * speed * Time.deltaTime);
+	}
+}
